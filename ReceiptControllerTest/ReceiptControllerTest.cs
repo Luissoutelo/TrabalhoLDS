b1ee95d3aed87a4e57d4fb1e60598c1146e2da54
Vasco Almeida <8210711@estg.ipp.pt>
2024-11-02 22:12:04 +0000

    ReceiptTest Start

---
+++ ReceiptControllerTest/ReceiptControllerTest.cs
@@ -0,0 +1,15 @@
+namespace ReceiptControllerTest
+{
+    [TestClass]
+    public class ReceiptControllerTest
+    {
+
+        private ReceiptsController _receipts;
+        private MachineHubContext _machineHubContext;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
